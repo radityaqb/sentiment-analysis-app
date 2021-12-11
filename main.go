9a4dda7aaf4efc36555e97026a53aa7e96aa957d
rhomac <rhomacahyanti@Rhomas-MacBook-Pro.local>
2021-12-11 12:16:38 +0700

    count most appearances words

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 var (
 	stopwords  sastrawi.Dictionary
 	dictionary sastrawi.Dictionary
+	queryWords sastrawi.Dictionary
 	stemmer    sastrawi.Stemmer
 
 	negativeWords []string
@@ -25,11 +27,16 @@ const (
 	Bad  bayesian.Class = "Bad"
 )
 
+var (
+	dictionaryQueryWords = []string{"goto", "ipo", "gojek", "tokopedia", "tokped", "toped"}
+)
+
 func init() {
 
 	stopwords = sastrawi.DefaultStopword()
 	dictionary = sastrawi.DefaultDictionary()
 	stemmer = sastrawi.NewStemmer(dictionary)
+	queryWords = sastrawi.NewDictionary(dictionaryQueryWords...)
 
 	// if 0 then Bad
 	// if 1 then Good
@@ -47,26 +54,20 @@ func main() {
 	negativeWords = ReadSourceReadonly("negative.txt", 0)
 	positiveWords = ReadSourceReadonly("positive.txt", 0)
 
-	// // remove newline
-	// re := regexp.MustCompile(`\r?\n`)
-	// for i := range s {
-	// 	s[i] = re.ReplaceAllString(s[i], " ")
-	// 	fmt.Println(s[i])
-	// }
-	// log.Fatal("done")
-
 	classifier.Learn(positiveWords, Good)
 	classifier.Learn(negativeWords, Bad)
 
 	for i := range s {
+		// stem
+		s[i] = stemmer.Stem(s[i])
 
 		// tokenize
 		result := []string{}
 		for _, word := range sastrawi.Tokenize(s[i]) {
-			if stopwords.Contains(word) {
+			if stopwords.Contains(word) || queryWords.Contains(word) {
 				continue
 			}
-			result = append(result, stemmer.Stem(word))
+			result = append(result, word)
 		}
 
 		s[i] = strings.Join(result, " ")
@@ -77,8 +78,10 @@ func main() {
 
 	// fmt.Println(s[1])
 	mapPolarity := make(map[int]int)
+
+	var positiveWords, negativeWords []string
+
 	for i := range s {
-		fmt.Println(s[i])
 
 		words := strings.Split(s[i], " ")
 		for i := range words {
@@ -87,6 +90,13 @@ func main() {
 
 		scores, likely, _ := classifier.LogScores(words)
 		fmt.Println(scores, " ", likely)
+
+		if likely == 1 {
+			positiveWords = append(positiveWords, words...)
+		} else {
+			negativeWords = append(negativeWords, words...)
+		}
+
 		mapPolarity[likely]++
 	}
 
@@ -95,6 +105,14 @@ func main() {
 	for k, v := range mapPolarity {
 		fmt.Println("key = ", k, " | ", v)
 	}
+
+	mapPositiveWords, mapNegativeWords := countPositiveAndNegativeWords(positiveWords, negativeWords)
+
+	mostAppearancesPositive := getMostAppearancesWords(10, mapPositiveWords)
+	mostAppearancesNegative := getMostAppearancesWords(10, mapNegativeWords)
+
+	fmt.Println("mostAppearancesPositive: ", mostAppearancesPositive)
+	fmt.Println("mostAppearancesNegative: ", mostAppearancesNegative)
 
 	// 	// probs, likely, _ := classifier.ProbScores(
 	// 	// 	strings.Split(s[i], " "),
@@ -109,4 +127,55 @@ func main() {
 	// 		"terlambat"},
 	// )
 	// fmt.Println(scores, " ", likely)
+}
+
+func countPositiveAndNegativeWords(positiveWords, negativeWords []string) (map[string]int, map[string]int) {
+	mapPositiveWords := make(map[string]int)
+	mapNegativeWords := make(map[string]int)
+
+	// breakdown positive and negative words
+	for _, positiveWord := range positiveWords {
+		if _, ok := mapPositiveWords[positiveWord]; ok {
+			mapPositiveWords[positiveWord]++
+			continue
+		}
+		mapPositiveWords[positiveWord]++
+	}
+
+	// breakdown negative and negative words
+	for _, negativeWord := range negativeWords {
+		if _, ok := mapPositiveWords[negativeWord]; ok {
+			mapNegativeWords[negativeWord]++
+			continue
+		}
+		mapNegativeWords[negativeWord]++
+	}
+
+	return mapPositiveWords, mapNegativeWords
+}
+
+func getMostAppearancesWords(rank int, mapWordFrequencies map[string]int) (mapMostAppearancesWord []string) {
+	type WordAppearances struct {
+		Key   string
+		Value int
+	}
+
+	var wordFrequencies []WordAppearances
+	for k, v := range mapWordFrequencies {
+		wordFrequencies = append(wordFrequencies, WordAppearances{k, v})
+	}
+
+	sort.Slice(wordFrequencies, func(i, j int) bool {
+		return wordFrequencies[i].Value > wordFrequencies[j].Value
+	})
+
+	for i := range wordFrequencies {
+		if i == 10 {
+			break
+		}
+
+		mapMostAppearancesWord = append(mapMostAppearancesWord, fmt.Sprintf("%s|%d", wordFrequencies[i].Key, wordFrequencies[i].Value))
+	}
+
+	return mapMostAppearancesWord
 }
